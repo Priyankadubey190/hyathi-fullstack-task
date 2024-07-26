833368540479047f3253512fda62391810d4cdfb
@priyankadubey0811 <priyankadubey0811@gmail.com>
2024-07-26 12:15:18 +0530

    getting add adopted and not adopted pokemon both

--- Backend/src/controllers/pokemon.controller.ts
+++ Backend/src/controllers/pokemon.controller.ts
@@ -9,23 +9,24 @@ const ITEMS_PER_PAGE = 5;
 export const getAllPokemon = async (req: AuthType, res: Response) => {
   try {
     const { page = 1 } = req.query;
-    const userId = req.currentUser?._id;
     const totalPokemon = await Pokemon.countDocuments();
 
-    const adopted: AdoptedPokemonModel | null = userId
-      ? await Adopted.findOne({
-          userId: new mongoose.Types.ObjectId(userId.toString()),
-        })
-      : null;
+    const adoptedPokemons: any = await Adopted.find(
+      {},
+      { "items.pokemon": 1, "items.adopted": 1 }
+    ).exec();
 
-    const pokemon: PokemonModel[] = await Pokemon.find()
+    const allPokemon: PokemonModel[] = await Pokemon.find()
       .limit(ITEMS_PER_PAGE)
       .skip((+page - 1) * ITEMS_PER_PAGE);
 
     const pokemonData = await Promise.all(
-      pokemon.map(async (p) => {
-        const isAdopted = adopted?.items.some(
-          (item) => item.pokemon.toString() === p._id.toString() && item.adopted
+      allPokemon.map(async (p) => {
+        const isAdopted = adoptedPokemons.some((ap: any) =>
+          ap.items.some(
+            (item: any) =>
+              item.pokemon.toString() === p._id.toString() && item.adopted
+          )
         );
         return {
           id: p._id.toString(),
@@ -34,7 +35,7 @@ export const getAllPokemon = async (req: AuthType, res: Response) => {
           healthStatus: p.healthStatus,
           feedTime: p.feedTime,
           lastFedAt: p.lastFedAt,
-          adopted: isAdopted || false,
+          adopted: isAdopted,
         };
       })
     );
